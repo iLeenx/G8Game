8a0dab1fb7fbfb45f0047ee5ab596a7d36334c28
iLeenx <LeenALshehrii@hotmail.com>
2025-12-18 18:20:44 +0300

    ray detect only 1 face and incorrectly

---
+++ Assets/_Leen/Rubik's Cube/CubeState.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class CubeState : MonoBehaviour
+{
+    // sides
+    public List<GameObject> front = new List<GameObject>();
+    public List<GameObject> back = new List<GameObject>();  
+    public List<GameObject> up = new List<GameObject>();
+    public List<GameObject> down = new List<GameObject>();
+    public List<GameObject> left = new List<GameObject>();
+    public List<GameObject> right = new List<GameObject>();
+
+
+    // Start is called once before the first execution
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Leen/Rubik's Cube/ReadCube.cs
+++ Assets/_Leen/Rubik's Cube/ReadCube.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class ReadCube : MonoBehaviour
 {
@@ -9,16 +11,34 @@ public class ReadCube : MonoBehaviour
     public Transform tFront;
     public Transform tBack;
 
+    private int layerMask = 1 << 6; // Layer 6 is assigned to "Faces"
+    private CubeState cubeState;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cubeState = FindFirstObjectByType<CubeState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        List<GameObject> facesHit = new List<GameObject>();
+        Vector3 ray = tFront.transform.position;
+        RaycastHit hit;
+
+        // does the ray intersect with any objects in the "Faces" layer?
+        if (Physics.Raycast(ray, tFront.right, out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(ray, tFront.right * hit.distance, Color.yellow);
+            facesHit.Add(hit.collider.gameObject);
+            Debug.Log("Front Face Hit: " + hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.DrawRay(ray, tFront.right * 1000, Color.red);
+            Debug.Log("Front Face: No hit");
+        }
+        cubeState.front = facesHit;
     }
 }
