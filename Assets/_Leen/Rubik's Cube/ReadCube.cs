730eedd3ac41097ef12c65df5700eb8add4e3691
iLeenx <LeenALshehrii@hotmail.com>
2025-12-18 17:47:50 +0300

    added rays game objects

---
+++ Assets/_Leen/Rubik's Cube/ReadCube.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ReadCube : MonoBehaviour
+{
+    public Transform tUp;
+    public Transform tDown;
+    public Transform tLeft;
+    public Transform tRight;
+    public Transform tFront;
+    public Transform tBack;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
