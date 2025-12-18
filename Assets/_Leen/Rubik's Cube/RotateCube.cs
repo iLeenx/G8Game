5900eaf609836cae61849264e3f3f645a9da549d
iLeenx <LeenALshehrii@hotmail.com>
2025-12-18 16:58:23 +0300

    cube rotate left and right

---
+++ Assets/_Leen/Rubik's Cube/RotateCube.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    private Vector2 firstPressPos;  // Position where the mouse button is first pressed
+    private Vector2 secondPressPos;  // Position where the mouse button is released
+    private Vector2 currentSwipe;  // Vector representing the swipe direction
+
+    public GameObject target;  // Reference to the cube GameObject
+    public float speed = 200f;  // Speed of rotation
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Swipe();
+        _RotateCube();
+    }
+
+    public void Swipe()
+    {
+        // Detects mouse button press
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            //Debug.Log("First Press Position: " + firstPressPos);
+        }
+
+        // Detects mouse button release
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Get the position where the mouse button is clicked
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            // Calculate the swipe direction from click positions to release positions
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            // Normalize the swipe vector to get direction only
+            currentSwipe.Normalize();
+
+            // Determine the swipe direction and rotate the cube accordingly            
+            if (LeftSwipe(currentSwipe))
+            {
+                Debug.Log("Left Swipe Detected");
+                target.transform.Rotate( 0, 90, 0, Space.World);
+            }
+            else if (RightSwipe(currentSwipe))
+            {
+                Debug.Log("Right Swipe Detected");
+                target.transform.Rotate( 0, -90, 0, Space.World);
+            }
+
+
+        }
+    }
+
+    bool LeftSwipe(Vector2 swipe)
+    {
+        return currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f;
+    }
+
+    bool RightSwipe(Vector2 swipe)
+    {
+        return currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f;
+    }
+
+    void _RotateCube()
+    {
+        // auto-rotate the cube slowly for visual effect
+        if (transform.rotation != target.transform.rotation)
+        {
+            var step = speed * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target.transform.rotation, step);
+        }
+    }
+}
