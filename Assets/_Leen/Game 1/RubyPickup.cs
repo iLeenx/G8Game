93ad9727909422d1dc4dda5045e746253158f893
iLeenx <LeenALshehrii@hotmail.com>
2025-12-27 23:14:27 +0300

    all 3 lasers work and you can steal the ruby

--- Assets/_Leen/Game 1/RubyPickup.cs
+++ Assets/_Leen/Game 1/RubyPickup.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class RubyPickup : MonoBehaviour
 {
     private bool canPickUp = false;
+    public GameObject pickUpPanel;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) canPickUp = true;
+        if (other.CompareTag("Player")) 
+            canPickUp = true;
+            pickUpPanel.SetActive(true);
+
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) canPickUp = false;
+        if (other.CompareTag("Player")) 
+            canPickUp = false;
+            pickUpPanel.SetActive(true);
     }
 
     void Update()
