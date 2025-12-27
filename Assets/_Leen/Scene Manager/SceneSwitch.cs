ff08059c84bb939e3b3ea39548dc091ad21597fb
iLeenx <LeenALshehrii@hotmail.com>
2025-12-27 20:17:26 +0300

    added main menu

---
+++ Assets/_Leen/Scene Manager/SceneSwitch.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void _GoMainMenu()
+    {
+        SceneManager.LoadScene("Main menu");
+
+        Cursor.visible = true;
+        Time.timeScale = 1f;
+    }
+
+    public void _GoSettings()
+    {
+        SceneManager.LoadScene("Settings");
+
+        Cursor.visible = true;
+        Time.timeScale = 1f;
+    }
+
+    public void _GoCredits()
+    {
+        SceneManager.LoadScene("Credits");
+
+        Cursor.visible = true;
+        Time.timeScale = 1f;
+    }
+
+    public void _GoHowTo()
+    {
+        SceneManager.LoadScene("How to");
+        Cursor.visible = true;
+        Time.timeScale = 1f;
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+
+
+}
