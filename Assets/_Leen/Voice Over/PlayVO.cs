3c2c747211d4fd7ef010b54c5b80100c811df046
iLeenx <LeenALshehrii@hotmail.com>
2025-12-28 19:50:44 +0300

    Voice Over Works!

---
+++ Assets/_Leen/Voice Over/PlayVO.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayVO : MonoBehaviour
+{
+    public VoiceLine introLine;
+    public VoiceLine doorTutorialLine;
+
+    public VoiceManager voiceManager;
+
+    void Start()
+    {
+        StartCoroutine(PlaySequence());
+    }
+
+    IEnumerator PlaySequence()
+    {
+        // play first line
+        voiceManager.PlayVoice(introLine);
+
+        // wait for that audio to finish + 3 seconds
+        yield return new WaitForSeconds(voiceManager.audioSource.clip.length + 3f);
+
+        // play next line
+        voiceManager.PlayVoice(doorTutorialLine);
+    }
+}
