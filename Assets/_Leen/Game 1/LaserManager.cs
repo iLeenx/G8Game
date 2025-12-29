494dbc8f63c3299eed03b627b9f1c15fb2a8920b
iLeenx <LeenALshehrii@hotmail.com>
2025-12-29 18:12:02 +0300

    flash light doesn't show if you don't solve puzzle 1

--- Assets/_Leen/Game 1/LaserManager.cs
+++ Assets/_Leen/Game 1/LaserManager.cs
@@ -4,6 +4,7 @@ public class LaserManager : MonoBehaviour
 {
     public GameObject[] lasers;    // 3 lasers
     public GameObject ruby;        // ruby object
+    public GameObject flashlight;  // flashlight object
 
     void Start()
     {
@@ -21,6 +22,25 @@ public class LaserManager : MonoBehaviour
             else
                 lasers[i].SetActive(true);
         }
+
+        // --- Flashlight logic ---
+        if (!PuzzleStateManager.IsLaserSolved(0))
+        {
+            // if laser 0 is NOT solved -> flashlight OFF
+            flashlight.SetActive(false);
+        }
+        else if (PuzzleStateManager.IsLaserSolved(0) && !PuzzleStateManager.IsLaserSolved(2))
+        {
+            // if laser 0 solved AND laser 2 NOT solved -> flashlight ON
+            flashlight.SetActive(true);
+        }
+        else if (PuzzleStateManager.IsLaserSolved(0) && PuzzleStateManager.IsLaserSolved(2))
+        {
+            // if laser 0 solved AND laser 2 solved -> flashlight OFF again
+            flashlight.SetActive(false);
+        }
+
+
 
         // Enable ruby if all lasers solved
         ruby.GetComponent<Collider>().enabled = PuzzleStateManager.AllLasersSolved();
