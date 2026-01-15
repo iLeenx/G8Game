7e022ef129ca233e835b4a0134e7891f82d364b4
iLeenx <LeenALshehrii@hotmail.com>
2026-01-15 17:38:06 +0300

    i think i finished everything ? you can solve anything and get a transition to final scen

--- Assets/_Leen/Game 1/Finale.cs
+++ Assets/_Leen/Game 1/Finale.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finale : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool won = false;
+
+    public GameObject FinaleCutSceneTrigger;
+    public GameObject EscapePanel;
+
+    private void Start()
     {
-        
+        FinaleCutSceneTrigger.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void WinningButtonActivation()
     {
-        
+        won = true;
+    }
+
+    private void Update()
+    {
+        if (won)
+        {
+            FinaleCutSceneTrigger.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && won)
+        {
+            SceneManager.LoadScene("final cutscene");
+        }
+    }
+
+    public void ButtonActivation()
+    {
+        won = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        EscapePanel.SetActive(false);
     }
 }
